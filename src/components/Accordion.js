9aa4bd564c4850b72636ce4cd8cf37e92d5fcf18
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-24 11:17:22 -0500

    adding on click helper method

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const Accordion = () => {
+const Accordion = ({ items }) => {
+
+    const onTitleClicked = (index) => {
+        console.log('Title clicked', index);
+    };
+
+    const renderedItems = items.map((item,index) => {
+        return(
+            <React.Fragment key={item.title}>
+                <div 
+                className="title active"
+                onClick={() => onTitleClicked(index)}>
+                    <i className="dropdown icon"></i>
+                    {item.title}
+                </div>
+                <div className="content active">
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        ); 
+    });
+
     return (
-        <div>
-            Accordion
+        <div className="ui styled accordion">
+            {renderedItems}
         </div>
     );
 };
