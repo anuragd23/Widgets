0e9a03188c19d9f764046cbfb4a876d1f265ef69
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-25 18:44:05 -0500

    Added useEffect for term

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Search = () => {
+
+    const [term, setTerm] = useState('');
+
+    useEffect(() => {
+        console.timeLog();
+    }, []);
+
     return(
         <div>
-            Search
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input
+                    value={term}
+                    onChange={e => setTerm(e.target.value)} 
+                    className="input"></input>                
+                </div>    
+            </div>            
         </div>
     );
 };
