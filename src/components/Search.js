26c55f877ab3b9abb803f6d87c6bd9b1c7f5e6b9
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-26 20:52:20 -0500

    storing results from wikipedia api call

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Search = () => {
 
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
-        console.timeLog();
-    }, []);
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+
+            setResults(data.query.search);
+        };
+
+        search();
+        
+    }, [term]);
 
     return(
         <div>
