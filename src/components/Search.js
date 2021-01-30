36c9964eeba409747501c004fb18a1c516b6ad2c
Anurag Das <anurag.das23.ad@gmail.com>
2021-01-29 19:22:04 -0500

    Added links to search results

--- src/components/Search.js
+++ src/components/Search.js
@@ -29,7 +29,12 @@ const Search = () => {
         return(
             <div key={result.pageid} className="item">
                 <div className="right floated content">
-                    <a className="ui button">Go</a>
+                    <a 
+                    className="ui button"
+                    href={`https://en.wikipedia.org/wiki/${result.title}`}
+                    >
+                        Go
+                    </a>
                 </div>
                 <div className="content"> 
                     <div className="header">
